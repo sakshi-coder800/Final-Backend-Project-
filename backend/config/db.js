8b1e9936d7ac95770e8777c84b7e7b6aef39f498
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-10-20 21:35:47 +0530

    Database connectivity

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,21 +6,14 @@ dotenv.config()
 
 const connectDB=async()=>{
     try {
-const conn = await mongoose.connect(process.env.DBConnection)       
-const conn = await mongoose.connect(process.env.MONGO_URI)       
+// const conn = await mongoose.connect(process.env.MONGO_URI)       
+const conn = await mongoose.connect(process.env.DBConnection , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})       
 console.log(`Connected to Mongodb Database ${conn.connection.host} `); 
     } catch (error) {
         console.log(`Database connection error ${error}`)
     }
 }
-
-
-//     try {
-// const  connDB = await mongoose.connect(()=>{
-        
-//     } catch (error) {
-        
-//     }
-// })
-
 export default connectDB;
