494f6495c093f5ae335d1f02af06ec98cdc561d3
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-10-21 00:12:20 +0530

    Middleware , LoginAuth , ControlAuth

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import { loginController, registerController, testController } from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 // Create a router object
 const router = express.Router();
@@ -12,7 +13,8 @@ router.post('/register', registerController);
 router.post("/login", loginController);
 
 // TEST ROUTE
-router.get("/test", testController); 
+
+router.get("/test", requireSignIn, isAdmin,  testController); 
 
 // Export the router
 export default router;
